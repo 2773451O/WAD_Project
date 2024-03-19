2645245fcd3aee0ce036fe32893971d9008c4f0a
Melissa Dorrian <melissadorrian04@gmail.com>
2024-03-19 13:21:08 +0000

    population script for recipes

--- populate_recipes.py
+++ populate_recipes.py
@@ -6,13 +6,13 @@ django.setup()
 from recipes.models import Recipe
 
 def populate():
-    recipes = [{'name':'Brownies', 'recipe':'add xyz cook long time yum'}, {'name':'Pizza', 'recipe':'add xyz cook long time yum'}, {'name':'Bread', 'recipe':'add xyz cook long time yum'}]
+    recipes = [{'name':'Brownies', 'ingredients':'flour egg idk', 'instructions':'add xyz cook long time yum', 'likes':0, 'picture':'cookies.jpg', 'views':24}, {'name':'Pizza', 'ingredients':'flour egg idk', 'instructions':'add xyz cook long time yum', 'likes':20, 'picture':'cookies.jpg', 'views':56}, {'name':'Bread', 'ingredients':'flour egg idk', 'instructions':'add xyz cook long time yum', 'likes':7, 'picture':'cookies.jpg', 'views':16}]
 
     for i in recipes:
-        add_recipe(i['name'], i['recipe'])
+        add_recipe(i['name'], i['ingredients'], i['instructions'], i['likes'], i['picture'], i['views'])
 
-def add_recipe(name,recipe):
-    rec = Recipe.objects.get_or_create(name = name, recipe = recipe)[0]
+def add_recipe(name,ingredients,instructions, likes, picture, views):
+    rec = Recipe.objects.get_or_create(name = name, ingredients = ingredients, instructions = instructions, likes=likes, picture=picture, views=views)[0]
     rec.save()
     return rec
 
